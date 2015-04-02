7ad679ccc506d7a87b211eed259635346edafdce
valemunoz <valeria@chilemap.cl>
2015-04-01 20:09:40 -0400

    update

--- js/funciones.js
+++ js/funciones.js
@@ -2,6 +2,7 @@ var path_query="http://obvii.net/obvii/markan/query.php";
 var MK_LON=0;
 var MK_LAT=0;
 var MK_ACUU=0;
+var MK_MAIL="";
 function openPopstatic(contenido)
 {
 	$("#cont_static").html(contenido);
@@ -9,12 +10,14 @@ function openPopstatic(contenido)
 }
 function deviceListo()
 {
-	
+	getEstadoUsuario();
 	document.getElementById("rut").focus();
+	
 }
 function errorPos()
 {
 	 openPopstatic("Error en la captura de coordenadas. Por favor revise su GPS");
+	 
 }
 function inicio()
 {
@@ -88,16 +91,61 @@ function loadUsuario(id,nick,lon,lat)
 				}
 			);
 }
-function opcMarca()
+function opcMarca(id_emp)
 {
-	openPopstatic('<div class=div_static><strong>Seleccione:</strong><br><br><input type="button" class="bottom_coment" onclick="marcar(0);" value="Entrada"><br><input type="button" class="bottom_coment" onclick="marcar(1);" value="Salida"></div>');
+	$.mobile.loading( 'show', {
+				text: 'Validando',
+				textVisible: true,
+				theme: 'a',
+				html: ""
+			});
+	
+		navigator.geolocation.getCurrentPosition (function (pos)
+		{
+			var lat = pos.coords.latitude;
+  		var lng = pos.coords.longitude;
+  		var accu=pos.coords.accuracy.toFixed(2);
+  		
+  		MK_LON=lng;
+  		MK_LAT=lat;
+  		MK_ACCU=accu;
+  		
+  		checkDistanciaMarca(id_emp);
+			
+		},errorGPS,{timeout:6000});	
+	
 	//alert("paso");
 	//mensaje("prueba","myPopup");
 	
 }
-function marcar(tipo)
+function checkDistanciaMarca(id_emp)
 {
-	openPopstatic("Marca realizada");
+	$("#output").load(path_query, 
+			{tipo:10, lon:MK_LON, lat:MK_LON, accu:MK_ACCU,id_emp:id_emp} 
+				,function(){				
+					$.mobile.loading( 'hide');		
+					
+					
+				}
+			);
+}
+function marcar(tipo,id_emp)
+{
+	$.mobile.loading( 'show', {
+				text: 'Marcando',
+				textVisible: true,
+				theme: 'a',
+				html: ""
+			});
+		$("#output").load(path_query, 
+			{tipo:9, es:tipo, id_emp:id_emp} 
+				,function(){				
+					$.mobile.loading( 'hide');		
+					openPopstatic("Marca realizada");
+					
+				}
+			);
+	
 }
 function loadMenu()
 {
@@ -158,6 +206,90 @@ function loadAyuda()
 				,function(){				
 					$.mobile.loading( 'hide');		
 					$('#contenido_sesion').trigger('create');
+					
+				}
+			);
+}
+function getEstadoUsuario()
+{
+	$.mobile.loading( 'show', {
+				text: 'Cargando',
+				textVisible: true,
+				theme: 'a',
+				html: ""
+			});
+		$("#output").load(path_query, 
+			{tipo:7} 
+				,function(){				
+					$.mobile.loading( 'hide');		
+					
+					
+				}
+			);
+	
+}
+function loadSesion()
+{
+	$.mobile.loading( 'show', {
+				text: 'Cargando',
+				textVisible: true,
+				theme: 'a',
+				html: ""
+			});
+	
+	$("#contenido_sesion").load(path_query, 
+			{tipo:8} 
+				,function(){				
+					$.mobile.loading( 'hide');		
+					$('#contenido_sesion').trigger('create');
+					
+				}
+			);
+}
+function errorGPS()
+{
+	openPopstatic('<div class=div_static>Ocurrio un error al obtener coordenas. Por favor intentelo nuevamente</div>');
+	$.mobile.loading( 'hide');		
+}
+function salir()
+{
+	$("#output").load(path_query, 
+			{tipo:11} 
+				,function(){				
+					window.location="index.html";
+					
+				}
+			);
+}
+function loadEnvioHistorial(tipo)
+{
+	$("#mypanel2").panel( "close" );
+	openPopstatic('<div class=div_static>Correo Electronico<br><input type="text" class=input_form id=mail_correo name=mail_correo value="'+MK_MAIL+'"><br><input type="button" onclick="historial('+tipo+');" class="bottom_coment" value="Enviar"></div>');
+	
+	
+}
+function historial(tipo)
+{
+	
+	$("#myPopup_static").popup("close");
+	setTimeout("sendHistorial("+tipo+");",1000);
+	
+}
+function sendHistorial(tipo)
+{
+	var mail=$.trim(document.getElementById("mail_correo").value);
+	$.mobile.loading( 'show', {
+				text: 'Enviando',
+				textVisible: true,
+				theme: 'a',
+				html: ""
+			});
+	
+	$("#output").load(path_query, 
+			{tipo:12, opc:tipo, mail:mail} 
+				,function(){				
+					$.mobile.loading( 'hide');		
+					openPopstatic('<div class=div_static>Historial enviado</div>');				
 					
 				}
 			);
